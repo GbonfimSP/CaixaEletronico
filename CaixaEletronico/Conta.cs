7634f57942b25f280b572996b85455b502d76fc5
Gabriel Bonfim <ghbonfim454@gmail.com>
2025-04-10 22:54:13 -0300

    Primeiro commit: Projeto Caixa Eletrônico

---
+++ CaixaEletronico/Conta.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    public class Conta
+    {
+        public double Saldo = 0;
+
+
+        public void MostrarSaldo()
+        {
+            Console.WriteLine($"Seu saldo atual é: R$ {Saldo.ToString("C")}");
+            Console.WriteLine("Digite qualquer tecla para seguir...");
+            Console.ReadKey();
+        }
+        public  void Depositar(double deposito)
+        {
+            if (deposito > 0)
+            {
+                Saldo += deposito;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valor depositado: " + deposito.ToString("C"));
+                Console.ResetColor();
+                Console.WriteLine("Saldo atual: " + this.Saldo.ToString("C"));
+                Console.WriteLine("Digite qualquer tecla para seguir...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido para deposito.");
+                Console.ReadKey();
+            }
+            
+        }
+        public void Sacar(double saque)
+        {
+            if(this.Saldo >= saque)
+            {
+                this.Saldo -= saque;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor sacado: " + saque.ToString("C"));
+                Console.ResetColor();
+                Console.WriteLine("Saldo atual: " + this.Saldo.ToString("C"));
+                Console.WriteLine("Digite qualquer tecla para seguir...");
+                Console.ReadKey();
+            }
+            else if (saque < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor invalido.");
+                Console.ResetColor();
+                Console.WriteLine("Digite qualquer tecla para seguir...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Saldo insuficiente.");
+                Console.ResetColor();
+                Console.WriteLine("Digite qualquer tecla para seguir...");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,82 @@
+﻿//Projeto pessoal caixa eletronico.
+using CaixaEletronico;
+
+bool loop = true;
+Conta MinhaConta = new();
+
+while (loop) 
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("><><><><><><><><><><><><><><><><><><><><><><><><");
+    Console.WriteLine("\n\n\n\t\tCaixa Eletrônico\n\n\n");
+    Console.WriteLine("><><><><><><><><><><><><><><><><><><><><><><><><\n");
+    Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"[{(int) menu.Saldo}] - Ver saldo");
+    Console.WriteLine($"[{(int) menu.Deposito}] - Depositar dinheiro");
+    Console.WriteLine($"[{(int) menu.Saque}] - Sacar dinheiro");
+    Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"[{(int) menu.Sair}] - Sair");
+    Console.ResetColor();
+
+    try { 
+        Console.Write("Escolha uma opção: ");
+        int opcao = Convert.ToInt32(Console.ReadLine());
+       
+
+        switch (opcao)
+        {
+            case (int) menu.Saldo:
+                MinhaConta.MostrarSaldo();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><");
+                Console.ResetColor();
+                break;
+            case (int)menu.Deposito:
+
+                Console.WriteLine("Digite valor para deposito:");
+                double deposito = Convert.ToDouble(Console.ReadLine());
+                MinhaConta.Depositar(deposito);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><");
+                Console.ResetColor();
+
+                break;
+            case (int)menu.Saque:
+                Console.WriteLine("Quanto deseja sacar? ");
+                double saque = Convert.ToDouble(Console.ReadLine());
+                MinhaConta.Sacar(saque);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><");
+                Console.ResetColor();
+                break;
+            case (int)menu.Sair:
+                Console.WriteLine("Muito obrigado! Volte sempre.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n><><><><><><><><><><><><><><><><><><><><><><><><");
+                Console.ResetColor();
+                loop = false;
+                break;
+            default:
+                Console.WriteLine("Opção invalida.");
+                break;
+        }
+    } catch (FormatException)
+    {
+        Console.WriteLine("Por favor, digite um numero valido.");
+    }
+    Console.WriteLine();
+
+    
+    
+
+}
+public enum menu
+{
+    Saldo = 1,
+    Deposito,
+    Saque,
+    Sair
+}
